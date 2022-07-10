bd9c11e0b6ae46a6d479fbd97b424cf4a320b4b7
Manjeet Singh <manjeetsinghmzn2002@gmail.com>
2022-07-10 16:02:59 +0530

    Initial commit

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,8 +1,10 @@
-import './ExpenseDate.css'
+import React from 'react';
 
-function ExpenseDate(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+import './ExpenseDate.css';
+
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
 
   return (
